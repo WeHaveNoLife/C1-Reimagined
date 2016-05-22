090dcbe4bce54cdb9f7ab5ced9495c35aefc7537
Chaitanya Paruchuri <Chaitanya.Paruchuri@cerner.com>
2016-05-21 22:45:47 -0500

    add customer data - created 1000

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 global.__base = __dirname + '/';
 var Customer =  require('./lib/customer.js');
 var apikey = 'e742fa65a65ce871eba63bd37b0c9908';
+var Chance = require("chance");
+var jsonfile = require('jsonfile')
+var chance = new Chance();
+var apikey = 'e742fa65a65ce871eba63bd37b0c9908';
+
+
+
 
 function customerDemo (apikey) {
 	var customerAccount = Customer.initWithKey(apikey);
@@ -17,11 +24,45 @@ function customerDemo (apikey) {
     "zip": "75061"
   }
 };
-			customerAccount.getCustomers().then(function (repos) {
-		        console.log("[Customer - Get All Customers] : Sample Customer: " + repos.length);
-		    })
-		    
-	    	customerAccount.addCustomers(newCustomer);
+	generateCustomerData(apikey);
+	customerAccount.getCustomers().then(function (repos) {
+        console.log("[Customer - Get All Customers] : Sample Customer: " + repos.length);
+				writeJsonToFile(repos);
+    });
+    
+	//customerAccount.addCustomers(newCustomer);
+	//console.log(chance.zip())
 }
 
 customerDemo(apikey);
+
+
+function generateCustomerData (apikey) {
+	var number = 1000;
+	var customerJson = {};
+	var customerAccount = Customer.initWithKey(apikey);
+	while(number > 0){
+		customerJson = {
+		  "first_name": chance.first(),
+		  "last_name": chance.last(),
+		  "address": {
+		    "street_number": chance.natural({min: 1, max: 2000}).toString(),
+		    "street_name": chance.street(),
+		    "city": chance.city(),
+		    "state": chance.state(),
+		    "zip": chance.zip()
+	 		}
+		}
+	 	customerAccount.addCustomers(customerJson);
+	 	number--;
+	}
+	
+}
+
+ 
+ function writeJsonToFile(data) {
+	var file = __base + '/tmp/data.json';
+	jsonfile.writeFile(file, data, function (err) {
+		console.error(err)
+	})
+ }; 
