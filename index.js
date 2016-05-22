036e5ecef93dc1911adf7c9df7caa486ed467489
Chaitanya Paruchuri <Chaitanya.Paruchuri@cerner.com>
2016-05-21 21:18:03 -0500

    stuff

--- index.js
+++ index.js
@@ -6,14 +6,22 @@ function customerDemo (apikey) {
 	var customerAccount = Customer.initWithKey(apikey);
 	//var custID = '55e94a6af8d8770528e60e64';
 	//var accID = '560072e0ce1cef140015e483';
-	customerAccount.getCustomers().then(function (repos) {
-		        console.log("[Customer - Get All Customers] : Sample Customer: " + repos[0].first_name);
+	var newCustomer = {
+  "first_name": "Chait",
+  "last_name": "Tanya",
+  "address": {
+    "street_number": "5030",
+    "street_name": "creek pkwy",
+    "city": "Irving",
+    "state": "tx",
+    "zip": "75061"
+  }
+};
+			customerAccount.getCustomers().then(function (repos) {
+		        console.log("[Customer - Get All Customers] : Sample Customer: " + repos.length);
 		    })
-		    .catch(function (err) {
-		        // API call failed...
-			}).finally(function () {
-	        // This is called after the request finishes either successful or not successful.
-	    	});
+		    
+	    	customerAccount.addCustomers(newCustomer);
 }
 
 customerDemo(apikey);
